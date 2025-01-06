90775d20d51255331a5bb29bce93cee17f26a4ff
Zelouchan <bad1dje@gmail.com>
2025-01-06 19:40:54 +0000

    written bunch of functions

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
+document.getElementById("reset").addEventListener("click", gridNum);
+// document.getElementById("colour").addEventListener("click", );
+// document.getElementById("random").addEventListener("click", );
+// document.getElementById("reset").addEventListener("click", );
+
 function gridNum() {
     let amount = prompt("How many grid squares would you like per side? Pick between 16 and 100")
+
+    if (amount < 16 || amount > 100 || isNaN(amount)) 
+      { alert("wrong! Pick a number between 16 and 100.");
+        return; } 
+
+    document.getElementById("container").innerHTML = "";
+    
     createGrid(amount);
     giveSize(amount);
 }
 
-function giveSize() {
-    let size = (100% / amount );
+function giveSize(amount) {
+    let size = (100 / amount) +"%";
     createGrid(size)
 }
 
-function createGrid() {
+function createGrid(amount, size) {
     let total = (amount * amount);
-    while (i = 0; i > total; i++);
-
-    var div = document.createElement("div");
+    for (let i = 0; i < total; i++) {
+    let div = document.createElement("div");
+    div.classList.add("grid");
     div.style.width = size;
     div.style.height = size;
+
     document.getElementById("container").appendChild(div);
-    // create div use function giveSize
+    }
+
+    document.getElementsByClassName("grid").addEventListener("mouseover", changeBackground);
+        function changeBackground() {
+            this.style.backgroundColor = "black";
+        }
 }
