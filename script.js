7adf2c42498ffc297cae80805bf09094d3c5e972
Zelouchan <bad1dje@gmail.com>
2025-01-07 21:10:28 +0000

    added comments to code

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// buttons: reset grid, pick colour, rainbow, clear to white
 document.getElementById("reset").addEventListener("click", gridNum);
 document.getElementById("colour").addEventListener("click", colourBackgroundBlack);
 document.getElementById("random").addEventListener("click", colourBackground);
 document.getElementById("clear").addEventListener("click", clearBg);
 
+// sets the background colours to white
 function clearBg() {
   let gridElements = document.querySelectorAll(".grid");
   gridElements.forEach((elem) => {
@@ -11,37 +13,40 @@ function clearBg() {
 });
 }
 
+// asks for amount of grid squares per side, empties parent container calls create grid
 function gridNum() {
-  let amount = prompt(
+  let answer = prompt(
     "How many grid squares would you like per side? Pick between 16 and 100"
   );
 
+  let amount = parseInt(answer);
+
   if (amount < 16 || amount > 100 || isNaN(amount)) {
     alert("wrong! Pick a number between 16 and 100.");
-    return;
-  }
+    return
+  } else {
 
-  document.getElementById("container").innerHTML = "";
+    document.getElementById("container").innerHTML = "";
 
+  // size to make sure all the divs are square and the same size
   let size = 100 / amount + "%";
   createGrid(amount, size);
-}
+}}
 
+// creates the grid. Takes user input amount, times it by itself and loops for that amount of times.
 function createGrid(amount, size) {
-  let total = amount * amount;
-
-  for (let i = 0; i < total; i++) {
+  for (let i = 0; i < (amount*amount); i++) {
     let div = document.createElement("div");
     div.classList.add("grid");
     div.style.width = size;
     div.style.height = size;
 
-    colourBackgroundBlack();
-
     document.getElementById("container").appendChild(div);
+    colourBackgroundBlack();
   }
 }
 
+// creates rainbow coloured background
 function colourBackground() {
   let gridElement = document.querySelectorAll(".grid");
 
@@ -52,6 +57,7 @@ function colourBackground() {
   });
 }
 
+// creates solid colour background
 function colourBackgroundBlack() {
   let gridElement = document.querySelectorAll(".grid");
 
@@ -62,6 +68,7 @@ function colourBackgroundBlack() {
   });
 }
 
+// generate random rgb
 function colourPicker() {
   let col1 = Math.floor(Math.random() * 256);
   let col2 = Math.floor(Math.random() * 256);
@@ -69,12 +76,13 @@ function colourPicker() {
   return `rgb(${col1}, ${col2}, ${col3})`;
 }
 
+// creates an emptry grid as soon as the page loads
 window.addEventListener("load", (event) => {
   for (let i = 0; i < 256; i++) {
     let div = document.createElement("div");
     div.classList.add("grid");
-    div.style.width = 100 / 16 + "%";
-    div.style.height = 100 / 16 + "%";
+    div.style.width = 100/16 + "%";
+    div.style.height = 100/16 + "%";
 
     colourBackgroundBlack();
 
